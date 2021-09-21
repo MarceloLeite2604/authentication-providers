7a73708bc556c2081a00489cd9c932b6dabcf75a
Marcelo Leite4 <mleite4@lenovo.com>
2021-09-21 16:36:14 -0300

    Create database load startup scripts

---
+++ docker/pg-beta/docker-entrypoint-initdb.d/load-dump.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+name="beta";
+dump_file="/docker-entrypoint-initdb.d/$name.dump";
+
+if [ ! -f "./$dump_file" ]; then
+  echo "Dump file \"$dump_file\" not found. Skipping load.";
+  exit 0;
+fi;
+
+echo "Loading dump file \"$dump_file\".";
+file_name=$(basename -- "$dump_file");
+database="${file_name%.*}";
+pg_restore -U "$POSTGRES_USER" -F c -d $database $dump_file;
+echo "Dump file \"$dump_file\" loaded on database.";
